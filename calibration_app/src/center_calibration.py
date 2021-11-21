4ee67511eae14a9702fe62ca6e677822f07474ee
Manuel Felipe Garcia Rincon <manuelf.garcia23@gmail.com>
2021-11-20 21:57:58 -0500

    initial center calibration

---
+++ calibration_app/src/center_calibration.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+from matplotlib.widgets import Cursor
+
+import matplotlib.cm
+
+
+def rotacion_xy(pd_X, pd_y, pd_Z):
+    xyz2 = np.zeros((len(pd_X), 3))
+    xyz2[:, 0] = pd_X
+    xyz2[:, 1] = pd_y
+    xyz2[:, 2] = pd_Z
+    R = np.matrix([[0.99948284,  0.00112413, - 0.03213697],
+                   [0.00112413,  0.99755653,  0.06985494],
+                   [0.03213697, - 0.06985494,  0.99703937]])
+    a_rotados = []
+    b_rotados = []
+    c_rotados = []
+    for punto in xyz2:
+        punto_rotado = ((R*np.asmatrix(punto).T).T)
+
+        a_rotados.append(punto_rotado[0, 0])
+        b_rotados.append(punto_rotado[0, 1])
+        c_rotados.append(punto_rotado[0, 2])
+
+    return a_rotados, b_rotados, c_rotados
+
+pcd_frame = pd.read_csv(
+    "/Users/manuelgarciarincon/Proyectos_python/personal/calibracion/centro_lidar/centro.csv")
+pcd_frame = pcd_frame.drop(
+    columns=['Points_m_XYZ:0',
+                'Points_m_XYZ:1',
+                'Points_m_XYZ:2',
+                'azimuth',
+                'distance_m',
+                'adjustedtime',
+                'timestamp',])
+pcd_frame = pcd_frame.rename(
+    columns={'X': 'Z', 'Z': 'X'})
+
+pcd_frame['X'], pcd_frame['Y'], pcd_frame['Z'] = rotacion_xy(
+    pcd_frame['X'], pcd_frame['Y'], pcd_frame['Z'])
+
+
+################# FILTER PCD POINTS BY DISTANCE #################
+pcd_frame = pcd_frame[(pcd_frame.Y >= 0.7)]  # 5
+pcd_frame = pcd_frame[(pcd_frame.Y <= 1.3)]  # 5
+pcd_frame = pcd_frame[(pcd_frame.Z <= 2)]  # 5'
+pcd_frame = pcd_frame[(pcd_frame.Z >= -0.53)]  # 5
+# pcd_frame = pcd_frame[(pcd_frame.Z >= -0.173)]  # 5'
+################# FILTER PCD POINTS BY DISTANCE #################
+print(pcd_frame['X'].max())
+print(pcd_frame['X'].idxmax())
+print(pcd_frame['laser_id'][pcd_frame['Z'].idxmax()])
+laser_filter=pcd_frame['laser_id'][pcd_frame['Z'].idxmax()]
+
+################# FILTER PCD POINTS BY LASER_ID #################
+centro_pcd = pcd_frame[(pcd_frame.laser_id == laser_filter)]  # 5
+################# FILTER PCD POINTS BY LASER_ID #################
+
+target_pcd = centro_pcd[(pcd_frame.Z >= -0.48)]  # 5'
+# target_pcd = centro_pcd[(pcd_frame.Z >= -0.53)]  # 5
+
+print('centro X:', pcd_frame['X'].mean())
+print('centro Y:', pcd_frame['Y'].mean())
+
+ax = plt.axes(projection='3d')
+
+ax.scatter(pcd_frame['X'], pcd_frame['Y'], pcd_frame['Z'], linewidth=0.01)
+ax.scatter(centro_pcd['X'], centro_pcd['Y'], centro_pcd['Z'], linewidth=2, color='red')
+ax.scatter(target_pcd['X'], target_pcd['Y'],
+           target_pcd['Z'], linewidth=6, color='green')
+
+ax.set_xlabel('x [m]')
+ax.set_ylabel('y [m]')
+ax.set_zlabel('z [m]')
+
+cursor = Cursor(ax, useblit=True, color='red', linewidth=2)
+# ax.view_init(0, 270)
+# ax.set_xlim(-0.5, 0.5)
+# ax.set_zlim(-0.5, 0.5)
+# ax.set_ylim(0, 1.32)
+
+# plt.axis('off')
+# plt.grid(b=None)
+plt.show()
